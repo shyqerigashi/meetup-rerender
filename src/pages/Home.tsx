78dbd55fb8a028bf0d053455b7133a04b3e2c8bf
Shyqeri Gashi <shyqeri.gashi1@gmail.com>
2022-03-01 21:55:52 +0100

    fix unnessesary rerendering issue in Home modal component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { HomeModal } from '../components/Home/HomeModal';
 import { useUIContext } from '../context/UIContext/UIContext';
 
 export const Home = () => {
   const { isModalOpen, toggleModal } = useUIContext();
-  return (
-    <div className="Home">
-      <HomeModal isModalOpen={isModalOpen} toggleModal={toggleModal} />
-    </div>
+  const HomeModalComponent = useMemo(
+    () => <HomeModal isModalOpen={isModalOpen} toggleModal={toggleModal} />,
+    [isModalOpen] // eslint-disable-line react-hooks/exhaustive-deps
   );
+  return <div className="Home">{HomeModalComponent}</div>;
 };

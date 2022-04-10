8051cbcf78ca26f694fa0ece6d9e2cb823e9d8f0
Leon Pahole <leon.pahole@gmail.com>
2022-04-10 15:56:20 +0200

    Memoize toggleModal function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
 import { HomeModal } from '../components/Home/HomeModal';
 import { useUIContext } from '../context/UIContext/UIContext';
 
 export const Home = () => {
   const { isModalOpen, toggleModal } = useUIContext();
+
+  /* 
+    HomeModal is still being rerendered, because a change in the context triggers a rerender in every subscribed component
+    When a component rerenders, this also triggers a rerender of all of it's children. 
+    This is why HomeModal rerenders, despite the fact that it's props are still the same. 
+  */
   return (
     <div className="Home">
       <HomeModal isModalOpen={isModalOpen} toggleModal={toggleModal} />
